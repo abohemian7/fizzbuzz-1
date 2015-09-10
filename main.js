2f977ab38bd0111f6377e714b2c5255bb9d1742c
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-09-09 21:17:37 -0400

    finished

--- main.js
+++ main.js
@@ -4,18 +4,33 @@
 
 $(function(){
 
-        var num = 0;
+        var num = 15
+            , returnVal = 0;
 
         var printNum = function(){
-            $('#output').html(num);
+            $('#output').html(returnVal);
         };
 
-        printNum();
-
         $('#my-button').click(function(){
+                var userNum = $("#userNum").val()
+                calcFizzBuzz((userNum || num))
                 printNum();
             }
         );
+
+        var calcFizzBuzz = function(num){
+            for(var i = 1;i <= num;i++){
+                (returnVal == 0)? returnVal = "<br>" + (i) + fizzy(i) : returnVal += "<br>" + (i) + fizzy(i);
+            }
+
+        }
+
+        var fizzy = function(aNum){
+            return (
+                ((aNum % 3)?"":"Fizz") +
+                ((aNum % 5)?"":"Buzz")
+            )
+        }
 
     }
 );
